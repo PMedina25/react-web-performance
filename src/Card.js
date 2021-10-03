0124c8df8695bc733adeda112303a157f0cc3957
Pablo Medina Rodríguez <pablo.medina@the-cocktail.com>
2021-10-03 16:18:50 +0200

    refactor(): App and Card components

--- src/Card.js
+++ src/Card.js
@@ -1,36 +1,37 @@
 import React from "react";
 
-const Card = React.memo(function Card({
-  card,
-  onDragStart,
-  onDragEnd,
-  onDoubleClick,
-}) {
-  return (
-    <div
-      onMouseDown={(ev) => {
-        const clickOffset = {
-          x: ev.clientX - parseFloat(ev.currentTarget.style.left),
-          y: ev.clientY - parseFloat(ev.currentTarget.style.top),
-        };
-        onDragStart(clickOffset);
-      }}
-      onMouseUp={onDragEnd}
-      onDoubleClick={onDoubleClick}
-      style={{
-        position: "absolute",
-        left: card.position.left,
-        top: card.position.top,
-        backgroundColor: "#fff",
-        padding: "25px",
-        cursor: "move",
-        userSelect: "none",
-      }}
-      key={card.id}
-    >
-      {card.label}
-    </div>
-  );
-});
+const Card = React.memo(
+  function Card({ card, onDragStart, onDragEnd, onDoubleClick }) {
+    return (
+      <div
+        onMouseDown={(ev) => {
+          const clickOffset = {
+            x: ev.clientX - parseFloat(ev.currentTarget.style.left),
+            y: ev.clientY - parseFloat(ev.currentTarget.style.top),
+          };
+          onDragStart(clickOffset);
+        }}
+        onMouseUp={onDragEnd}
+        onDoubleClick={onDoubleClick}
+        style={{
+          position: "absolute",
+          left: card.position.left,
+          top: card.position.top,
+          backgroundColor: "#fff",
+          padding: "25px",
+          cursor: "move",
+          userSelect: "none",
+        }}
+        key={card.id}
+      >
+        {card.label}
+      </div>
+    );
+  },
+  ({ card: oldCard }, { card: newCard }) =>
+    oldCard.id === newCard.id &&
+    oldCard.position.left === newCard.position.left &&
+    oldCard.position.top === newCard.position.top
+);
 
 export default Card;
